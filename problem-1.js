93a3f78cd1e371ef040352cd5ec665669bf584ca
justinejeciel-design <justine.jeciel@olivarezcollegetagaytay.edu.ph>
2025-10-08 11:10:46 +0800

    New updates

---
+++ problem-1.js
@@ -0,0 +1,5 @@
+const num1 = 25;const num2 = 5;const sum = num1 + num2;const difference = num1 - num2;
+console.log(`The first number is: ${num1}\
+  \nThe second number is: ${num2}\
+  \nThe sum of ${num1} and ${num2} is: ${sum}\
+  \nThe Difference of ${num1} and ${num2} is: ${difference}`);
---
+++ problem-2.js
@@ -0,0 +1,4 @@
+const fahrenheit = 100
+const celsius = (fahrenheit - 32) * 5 / 9
+
+console.log(`${fahrenheit} degrees Fahrenheit is ${celsius} degrees Celsius`)
