cb9c97ccac67babfcd9818cb33f94ba80508b153
justinejeciel-design <justine.jeciel@olivarezcollegetagaytay.edu.ph>
2025-10-08 10:46:45 +0800

    new commit

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("This is for those who");
+// console.log("This is for those who");
+const PI = 3.14159;
+let radius = 5;
+let area = PI * radius * radius;
+
+console.log("The radius of the circle is: " + radius);
+console.log("The value of PI is: " + PI);
+console.log("The area of the circle is: " + area);
